91ab1aa3f1a84def6ef04dcd5458b3a85034fc3c
LordPounds1 <91436904+LordPounds1@users.noreply.github.com>
2023-09-12 22:07:02 +0600

    Add project files.

---
+++ Mod_2_12_b/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Mod_2_12_b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение b (b > 10): ");
+            int b = int.Parse(Console.ReadLine());
+
+            if (b > 10)
+            {
+                Console.WriteLine($"Квадраты всех чисел от 10 до {b}:");
+                for (int i = 10; i <= b; i++)
+                {
+                    Console.WriteLine(i * i);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Значение b должно быть больше 10.");
+            }
+        }
+    }
+}
